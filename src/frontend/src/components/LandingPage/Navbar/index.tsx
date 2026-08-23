da2f40e14a007e97680b949c955708b33cc7bfb3
Jason Murray <jason@chaosaffe.io>
2026-08-23 13:31:02 -0700

    fix(frontend): fix landing page top bar on mobile
    Version tag + Tutorials/Documentation/Supported Drones/language links had no mobile breakpoint, colliding at 390px. Hides the desktop link row below sm, demotes the version tag, and collapses everything into the existing hamburger+Drawer pattern already used by the authenticated app's Navbar.
    
    Signed-off-by: Jason Murray <jason@chaosaffe.io>

--- src/frontend/src/components/LandingPage/Navbar/index.tsx
+++ src/frontend/src/components/LandingPage/Navbar/index.tsx
@@ -1,6 +1,9 @@
+import { useState } from 'react';
 import { FlexRow } from '@Components/common/Layouts';
 import { Link } from 'react-router-dom';
 import LanguageSwitcherLanding from '@Components/common/LanguageSwitcherLanding';
+import Drawer from '@Components/common/Drawer';
+import Icon from '@Components/common/Icon';
 import { getRuntimeConfig } from '@/runtimeConfig';
 import { m } from '@/paraglide/messages';
 import packageInfo from '../../../../package.json';
@@ -22,6 +25,7 @@ if (AUTH_PROVIDER === 'hanko') {
 export default function Navbar() {
   // Return URL for hanko-auth callback
   const hankoReturnUrl = `${FRONTEND_URL}/hanko-auth`;
+  const [drawerOpen, setDrawerOpen] = useState(false);
 
   return (
     <header>
@@ -38,17 +42,17 @@ export default function Navbar() {
       )}
       <FlexRow
         gap={10}
-        className="naxatw-justify-between naxatw-border-landing-white naxatw-bg-landing-red naxatw-px-2 naxatw-py-2 naxatw-text-xs naxatw-text-landing-white sm:naxatw-px-20"
+        className="naxatw-items-center naxatw-justify-between naxatw-border-landing-white naxatw-bg-landing-red naxatw-px-2 naxatw-py-2 naxatw-text-xs naxatw-text-landing-white sm:naxatw-px-20"
       >
-        <span className="naxatw-whitespace-nowrap naxatw-opacity-75">
+        <span className="naxatw-hidden naxatw-whitespace-nowrap naxatw-opacity-75 sm:naxatw-inline">
           v{packageInfo.version}
         </span>
+
+        {/* Desktop link row */}
         <FlexRow
           gap={5}
-          className="naxatw-h-fit naxatw-flex-nowrap naxatw-items-center naxatw-text-xs naxatw-leading-none"
+          className="naxatw-hidden naxatw-h-fit naxatw-flex-nowrap naxatw-items-center naxatw-text-xs naxatw-leading-none sm:naxatw-flex"
         >
-          {/* <p className="naxatw-border-r naxatw-pr-3">About</p>
-          <p className="naxatw-border-r naxatw-pr-3">FAQs</p> */}
           <Link
             className="naxatw-whitespace-nowrap naxatw-border-r naxatw-pr-3 hover:naxatw-underline"
             to="/tutorials"
@@ -75,7 +79,58 @@ export default function Navbar() {
           </a>
           <LanguageSwitcherLanding />
         </FlexRow>
+
+        {/* Mobile hamburger */}
+        <button
+          type="button"
+          className="naxatw-flex naxatw-items-center sm:naxatw-hidden"
+          onClick={() => setDrawerOpen(true)}
+          aria-label={m.nav_open_menu_aria_label()}
+        >
+          <Icon name="menu" className="naxatw-text-landing-white" />
+        </button>
       </FlexRow>
+
+      <Drawer open={drawerOpen} onClose={() => setDrawerOpen(false)}>
+        <div className="naxatw-flex naxatw-flex-col naxatw-gap-4 naxatw-p-4">
+          <div className="naxatw-flex naxatw-items-center naxatw-justify-between">
+            <span className="naxatw-text-xs naxatw-text-grey-500">
+              v{packageInfo.version}
+            </span>
+            <button
+              type="button"
+              onClick={() => setDrawerOpen(false)}
+              aria-label={m.nav_close_menu_aria_label()}
+            >
+              <Icon name="close" />
+            </button>
+          </div>
+          <div className="naxatw-flex naxatw-flex-col naxatw-gap-3 naxatw-text-body-btn">
+            <Link
+              onClick={() => setDrawerOpen(false)}
+              to="/tutorials"
+              className="hover:naxatw-underline"
+            >
+              {m.landing_navbar_tutorials()}
+            </Link>
+            <a
+              href="https://docs.drone.hotosm.org "
+              className="hover:naxatw-underline"
+            >
+              {m.landing_navbar_documentation()}
+            </a>
+            <a
+              href="https://github.com/hotosm/Drone-TM/#drone-support"
+              className="hover:naxatw-underline"
+            >
+              {m.landing_navbar_supported_drones()}
+            </a>
+          </div>
+          <div className="naxatw-border-t naxatw-border-grey-300 naxatw-pt-4">
+            <LanguageSwitcherLanding className="naxatw-text-grey-800" />
+          </div>
+        </div>
+      </Drawer>
     </header>
   );
 }
